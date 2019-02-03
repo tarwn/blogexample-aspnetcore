c6fd5fb9ef2f1d189c1e888bf7947dc274789992
Eli Weinstock-Herman <tarwn@tiernok.com>
2019-02-03 08:27:09 -0500

    Example 2.0 and 2.2 projects

---
+++ dotnet-2.0/BlogExample.Web.Tests/Controllers/SampleDataControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using BlogExample.Web.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static BlogExample.Web.Controllers.SampleDataController;
+
+namespace BlogExample.Web.Tests.Controllers
+{
+    [TestFixture]
+    public class SampleDataControllerTests
+    {
+        private SampleDataController _controller;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _controller = new SampleDataController()
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = new DefaultHttpContext()
+                    {
+                        // set context properties here
+                    }
+                }
+            };
+        }
+
+        [Test]
+        public void WeatherForecasts_NoParams_ReturnsAllForecasts()
+        {
+            var result = _controller.WeatherForecasts();
+
+            Assert.IsInstanceOf<OkObjectResult>(result);
+            var okResult = (OkObjectResult)result;
+            Assert.IsInstanceOf<List<WeatherForecast>>(okResult.Value);
+            var data = (List<WeatherForecast>)okResult.Value;
+        }
+    }
+}
