a4fbff924a28a4ffb1ad4c47e4533c9bd7134fa9
Eli weinstock-Herman <eli@tiernok.com>
2022-04-03 11:05:46 -0400

    Fix ASP.Net 2-4s latency on file loads

--- dotnet-6/multi-webpack-entrypoints/src/reactapp/scripts/env.config.js
+++ dotnet-6/multi-webpack-entrypoints/src/reactapp/scripts/env.config.js
@@ -2,13 +2,14 @@ const fs = require("fs");
 const path = require("path");
 
 const isProd = process.env.NODE_ENV === "production";
+const isHostedThroughVS = (!!process.env.ASPNET_PORT);
 const rootPath = fs.realpathSync(process.cwd());
 module.exports = {
   isProd,
   rootPath,
 
   devProtocol: "http",
-  devHost: process.env.HOST || "localhost",
+  devHost: process.env.HOST || (isHostedThroughVS ? "[::1]" : "localhost"),
   devPort: process.env.PORT || 8881,
   clientWebSocketHost: "0.0.0.0",
   clientWebSocketPort: process.env.ASPNET_PORT || process.env.PORT || 8881,
